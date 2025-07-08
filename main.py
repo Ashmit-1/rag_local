8000afd82972d2b883f1960998111adb766656d4
dev_das <dasashmit16@gmail.com>
2025-07-08 18:30:57 +0530

    successful implementation of RAG

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
+from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage, HumanMessage
 
 file_path = '''data\\Arjun, the nature lover.pdf'''
 
@@ -21,7 +23,7 @@ def load_document():
 def chunk_split(documents):
     try:
         texts = []
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=100)
         id_count = 0
         for i in documents:
             text = text_splitter.split_text(i.page_content)
@@ -66,10 +68,47 @@ def embed_store(documents, collection_name="test_collection"):
     except Exception as e:
         print("Exception occured while embedding and storing")
 
+def retrieve_docs_db(vector_store, question):
+    try:
+        retriever = vector_store.as_retriever(search_kwargs={'k':3})
+        fetched_docs = retriever.invoke(question)
+        # for i, data in enumerate( fetched_docs):
+        #     print(f"Document {i}:")
+        #     print(data.page_content)
+        #     print("\n\n")
+        return fetched_docs
+    except Exception as e:
+        print("Exception during retrieval of related docs")
+
+def response_from_llm(relevant_docs, question):
+    try:
+        llm = ChatOllama(model="gemma3:1b")
+        system_prompt = "You are a question-answer bot. You have one simple job, you need to answer to questions consicely based on the following context. Context: {context}.\n\n\n\n Special Instructions: Remember your answer should be brief and it should be to the point to the question. If you do not find the answer in the context provided tell that you don't know the answer as it is not present in the context. "
+
+        context_str = "".join(d.page_content for d in relevant_docs)
+        contextual_prompt = system_prompt.format(context=context_str)
+
+        response = llm.invoke([SystemMessage(content=contextual_prompt), 
+                               HumanMessage(content=question)])
+        return response
+    except Exception as e:
+        print("Exception in getting response from llm")
+
 def main():
     documents = load_document()
     splitted_docs = chunk_split(documents)
     vector_store = embed_store(splitted_docs)
+    questions = ["What was Arjun known for in the village?", 
+                 "What did Arjun find in the abundunt hut?",
+                 "What did Arjun do after learning from the book?", 
+                 "How did the village change due to Arjun's efforts?",
+                 "What lesson did Arjun teach the villagers?"]
+    for question in questions:
+        relevant_docs = retrieve_docs_db(vector_store, question)
+        answer = response_from_llm(relevant_docs, question)
+        print("Question: " + question)
+        print("Answer: " + answer.content)
+        print("\n\n")
     
 
 
