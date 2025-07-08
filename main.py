04501bb1d0e792a2dc80bf35b07e80ae0e4645eb
dev_das <dasashmit16@gmail.com>
2025-07-08 15:00:56 +0530

    implement simple document loading using langchain

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+file_path = '''data\\Arjun, the nature lover.pdf'''
+
 def main():
-    print("Hello from rag-local!")
+    loader = PyPDFLoader(file_path=file_path)
+    pages = []
+    for page in loader.lazy_load():
+        pages.append(page)
+    print(f"{pages[0].metadata}\n")
+    print(pages[0].page_content)    
 
 
 if __name__ == "__main__":
