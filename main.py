aa35e34efafed2d366e535b705418d27719f9597
dev_das <dasashmit16@gmail.com>
2025-07-10 16:47:32 +0530

    successfully implement the auto sync function and restructured the code to make it more efficient

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from uuid import uuid4
 
 file_path = '''data'''
 db_dir = '''./chroma_langchain_db'''
-recreate = True
+recreate = False
 
 def load_document(file_path):
     try:
@@ -60,14 +60,14 @@ def chunk_split(documents):
         sys.exit(1)
 
 
-def embed_store(documents, collection_name="test_collection", model_name = "bge-small-en-v1.5"):
+def embed_store(collection_name = "test_collection", model_name = "bge-small-en-v1.5"):
     try:
         embeddings = HuggingFaceEmbeddings(
             model_name=f"BAAI/{model_name}", 
             model_kwargs = {'device': 'cpu'},
             encode_kwargs={"normalize_embeddings": True})
         if not os.path.exists(db_dir) or recreate:
-            print("Creating new database.....\n\n")
+            print("Creating new database.....\n")
             if os.path.exists(db_dir):
                 shutil.rmtree(db_dir)
             vector_store = Chroma(
@@ -76,10 +76,10 @@ def embed_store(documents, collection_name="test_collection", model_name = "bge-
                 persist_directory="./chroma_langchain_db"
             )
 
-            uuids = [str(uuid4()) for _ in range(len(documents))]
-            vector_store.add_documents(documents=documents, ids=uuids)
+            # uuids = [str(uuid4()) for _ in range(len(documents))]
+            # vector_store.add_documents(documents=documents, ids=uuids)
         else:
-            print("Old database found !!! \n\n")
+            print("Old database found !!! \n")
             vector_store = Chroma(
                 collection_name=collection_name,
                 embedding_function=embeddings,
@@ -93,13 +93,17 @@ def embed_store(documents, collection_name="test_collection", model_name = "bge-
 
 def auto_sync_db(vector_store):
     try:
-        data_dir_set = set()
-        database_set = set()
 
+        print("Syncing....")
+
+        ### this is for getting the files which are there in the directory
+        data_dir_set = set()
         for filename in os.listdir(file_path):
             if filename.endswith(".pdf"):
-                data_dir_set.add(filename)
+                data_dir_set.add(os.path.join(file_path, filename))
         
+        ### this is for getting the files already present in the database
+        database_set = set()
         docs = vector_store.get()
         source_id_dict = {}
         for index in range(len(docs["ids"])):
@@ -113,8 +117,32 @@ def auto_sync_db(vector_store):
         # for i in source_id_dict:
         #     print(f"{i} : {source_id_dict[i]}\n\n")
 
-        sys.exit(1)
+
+        ### adding newly added files to the database
+        to_add = data_dir_set - database_set
+        try:
+            for new_file in to_add:
+                print(f"Adding {new_file} in the database....")
+                documents = load_document(new_file)
+                splitted_docs = chunk_split(documents)
+                uuids = [str(uuid4()) for _ in range(len(splitted_docs))]
+                vector_store.add_documents(documents=splitted_docs, ids=uuids)
+        except Exception as e:
+            print("Exception occured from the sync function while adding new files to the database. " + str(e))
+            sys.exit(1)
+
         
+        ### deleting data in order to sync with the data directory
+        to_delete = database_set - data_dir_set
+        try:
+            for deleted_file in to_delete:
+                print(f"Deleting {deleted_file} from the database...")
+                vector_store.delete(ids=source_id_dict[deleted_file])
+        except Exception as e:
+            print("Exception occured from the sync function while deleting data from database. " + str(e))
+        
+        print("Successfully synced !!")
+
     except Exception as e:
         print("Exception occured while smart sync. " + str(e))
         sys.exit(1)
@@ -123,16 +151,18 @@ def retrieve_docs_db(vector_store, question):
     try:
         retriever = vector_store.as_retriever(search_type="mmr", search_kwargs={'k':5})
         fetched_docs = retriever.invoke(question)
+
         # for i, data in enumerate( fetched_docs):
         #     print(f"Document {i}:")
         #     print(data.page_content)
         #     print("\n\n")
+        
         return fetched_docs
     except Exception as e:
         print("Exception during retrieval of related docs")
         sys.exit(1)
 
-def response_from_llm(relevant_docs, question):
+def response_from_llm(relevant_docs, questions):
     try:
         llm = ChatOllama(model="gemma3:1b")
         system_prompt = "You are a question-answer bot. You have one simple job, you need to answer to questions consicely based on the following context. Context: {context}.\n\n\n\n Special Instructions: Remember your answer should be brief and it should be to the point to the question. If you do not find the answer in the context provided tell that you don't know the answer as it is not present in the context. "
@@ -141,21 +171,14 @@ def response_from_llm(relevant_docs, question):
         contextual_prompt = system_prompt.format(context=context_str)
 
         response = llm.invoke([SystemMessage(content=contextual_prompt), 
-                               HumanMessage(content=question)])
+                               HumanMessage(content=questions)])
         return response
     except Exception as e:
-        print("Exception in getting response from llm")
+        print("Exception in getting response from llm. " + str(e))
         sys.exit(1)
 
 def main():
-    if not os.path.exists(db_dir) or recreate:
-        documents = load_document(file_path)
-        splitted_docs = chunk_split(documents)
-    else:
-        splitted_docs=None
-
-        
-    vector_store = embed_store(splitted_docs)
+    vector_store = embed_store()
     auto_sync_db(vector_store)
 
     questions = ["What was Arjun known for in the village?", 
